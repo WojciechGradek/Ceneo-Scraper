0a3d7ca74da2c29ba95d5a574592f556a519c7ea
WojciechGradek <s219530@student.uek.krakow.pl>
2021-05-30 16:28:26 +0200

    30.05.2021

--- scraper.py
+++ scraper.py
@@ -1,45 +1,44 @@
-from typing import Type
-import requests
 from bs4 import BeautifulSoup
+import requests
 import json
 
-def extract_element(dom_tree, selector, attribute=None, return_List=None):
+def extract_element(dom_tree, selector, attribute=None):
     try:
-        if isinstance (attribute, str):
+        if isinstance(attribute, str):
             return dom_tree.select_one(selector)[attribute].strip()
-        if isinstance (attribute, list):
+        if isinstance(attribute, list):
             return [e.text.strip() for e in dom_tree.select(selector)]
         return dom_tree.select_one(selector).text.strip()
     except (AttributeError, TypeError):
         return None
+
 all_reviews = []
 service_url = "https://www.ceneo.pl"
-next_page = "/71299208#tab=reviews"
+next_page = "/39562616#tab=reviews"
+# next_page = "/71299209#tab=reviews"
 while next_page:
     respons = requests.get(service_url + next_page)
-
     page_dom = BeautifulSoup(respons.text, 'html.parser')
-
-    #print(page_dom.prettify())
-
     reviews = page_dom.select("div.js_product-review")
-
-for review in reviews:
-    single_review = {
-            "review_id": review["data-entry-id"]
+    for review in reviews:
+        single_review = {
+            "review_id": review["data-entry-id"],
             "author": extract_element(review, "span.user-post__author-name"),
             "recommendation": extract_element(review, "span.user-post__author-recomendation"),
             "stars": extract_element(review, "span.user-post__score-count"),
             "content": extract_element(review, "div.user-post__text"),
-            "pros": extract_element(review, "div.review-feature__title--positives ~ div.review-feature__item", []),
+            "pros": extract_element(review,"div.review-feature__title--positives ~ div.review-feature__item", []),
             "cons": extract_element(review, "div.review-feature__title--negatives ~ div.review-feature__item", []),
             "useful": extract_element(review, "button.vote-yes"),
             "useless": extract_element(review, "button.vote-no"),
-            "review_date": extract_element(review, "span.user-post__published > time:nth-child(1)")"datetime",
-             "purchase_date": extract_element(review, "span.user-post__published > time:nth-child(2)")"datetime"
+            "purchased": extract_element(review, "div.review-pz"),
+            "review_date": extract_element(review, "span.user-post__published > time:nth-child(1)", "datetime"),
+            "purchase_date": extract_element(review, "span.user-post__published > time:nth-child(2)", "datetime")
         }
         all_reviews.append(single_review)
-    next_page = extract_element(page_dom, "a.pagination__next", "href")
-with open("opinions/71299208.json", "w") as jf:
-    print(json.dumps(all_reviews, ensure_ascii=False, indent=4))
-#print(review_id, author, recommendation, stars, content, pros, cons, useful, useless, purchase_date, review_date)
+    next_page = extract_element(page_dom,"a.pagination__next", "href")
+
+with open("opinions/39562616.json", "w", encoding="UTF-8") as jf:
+    json.dump(all_reviews, jf, ensure_ascii=False, indent=4)
+
+# print(json.dumps(all_reviews, ensure_ascii=False, indent=4))
